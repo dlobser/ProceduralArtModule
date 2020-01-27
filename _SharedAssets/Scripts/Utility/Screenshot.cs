1eb943b6f92873646e8c73cca8a3760fdb9d6801
david lobser <davidlobser@yahoo.com>
2020-01-27 16:12:02 -0500

    added screenshot

---
+++ _SharedAssets/Scripts/Utility/Screenshot.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System;
+
+public class Screenshot : MonoBehaviour
+{
+    public string screenshotName = "screenshot";
+    public int superSize = 1;
+
+	private void Update()
+	{
+        if(Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.S)){
+            ScreenCapture.CaptureScreenshot(screenshotName + DateTime.Now.ToString("_MM_dd_HH_mm_ss"), superSize);
+        }
+	}
+}
