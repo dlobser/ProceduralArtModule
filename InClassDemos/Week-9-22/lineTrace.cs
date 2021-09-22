05d44cba2377d3015229f5f4cb6a6b150b64edf6
david lobser <davidlobser@yahoo.com>
2021-09-22 14:07:38 -0600

    line trace

---
+++ InClassDemos/Week-9-22/lineTrace.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lineTrace : MonoBehaviour
+{
+    public Transform moveUp;
+    public Transform leftAndRight;
+
+    public float frequency;
+    public float amplitude;
+
+    public float frequency2;
+    public float amplitude2;
+
+    public float moveUpSpeed;
+
+    void Update()
+    {
+        moveUp.localPosition = new Vector3(
+            Mathf.Sin(Time.time*frequency) * amplitude, 
+            Time.time * moveUpSpeed, 
+            0);
+
+        leftAndRight.localEulerAngles = new Vector3(0, 0, 
+            Mathf.Sin(Time.time * frequency2) * amplitude2);
+
+    }
+}
